8e86ce28911fe2bd23f325c08b4fa8a1cf137169
Adrien <adrien@arccoza.com>
2020-01-25 19:53:59 +0900

    Add edge method and update draw method with corner rounding

--- src/shape.js
+++ src/shape.js
@@ -9,7 +9,7 @@ export class Shape {
       this.pts.push(v)
     }
     this.transform = new Matrix([1, 0, 0, 1, 0, 0])
-    this.isClosed = true
+    this.isClosed = false
   }
 
   get(idx) {
@@ -18,8 +18,16 @@ export class Shape {
     return tfm.isIdentity || pt == null ? pt : pt.matrixTransform(tfm)
   }
 
+  edge(s, e) {
+    const pts = this.pts, len = pts.length, tfm = this.transform
+    s = (s < 0 ? len + s : s) % len
+    e = (e < 0 ? len + e : e) % len
+    const sl = e < s ? [...pts.slice(s), ...pts.slice(0, e)] : pts.slice(s, e)
+    return !tfm.isIdentity ? sl.map(pt => pt.matrixTransform(tfm)) : sl
+  }
+
   draw(ctx) {
-    const pts = this.pts, isClosed = this.isClosed, wedge = new Array(3)
+    const pts = this.pts, len = pts.length, isClosed = this.isClosed
     
     // If the ctx is a Path2D it won't have beginPath, check before call
     ctx.beginPath && ctx.beginPath()
@@ -29,14 +37,13 @@ export class Shape {
       pts.forEach((p, i) => i == 0 ? ctx.moveTo(p.x, p.y) : ctx.lineTo(p.x, p.y))
       return isClosed && ctx.closePath(), ctx
     }
-    
-    
-    var p = this.get(0)
-    ctx.moveTo(p.x, p.y)
-    for (var i = 0; i < this.pts.length; i++) {
-      p = this.get(i)
-      ctx.lineTo(p.x, p.y)
-      // ctx.arcTo(p.x, p.y, p.x, p.y, 50)
+        
+    for (var i = 0; i < len; i++) {
+      var w = this.edge(i-1, i+2), r = this.rad[0]
+      var [a1, c1, c2, a2] = w[1].round(w[0], w[2], r)
+
+      i ? ctx.lineTo(a1.x, a1.y) : ctx.moveTo(a1.x, a1.y)
+      ctx.bezierCurveTo(c1.x, c1.y, c2.x, c2.y, a2.x, a2.y)
     }
     
     return isClosed && ctx.closePath(), ctx
